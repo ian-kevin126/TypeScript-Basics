db56f92d87884ea4b4488d50743a44870b0bc0d2
ian_kevin126 <ian_kevin126@126.com>
2021-08-06 22:05:29 +0800

    feat: add interface

--- src/js/index.ts
+++ src/js/index.ts
@@ -18,3 +18,59 @@ function showMyName1({ name, age, lastName }: MyBrother): void {
 }
 
 // 2，索引签名
+interface FullName {
+  [propName: string]: string
+}
+
+const obj1: FullName = {
+  name: "kevin",
+  lastName: 'liao',
+  // addr: 1212,  报错
+  false: '1212',  // 不报错，false最终会被转化为string类型
+}
+// console.log({ 1: 'abc', '1': '123' }) // 这里面的第一个1最后都会被转化为string类型，所最终输出{'1': '123'}
+
+interface NumArray {
+  [propName: number]: string
+}
+
+// 还可来声明数组
+const _numArr: NumArray = ['a', 'b', 'c'];
+console.log(_numArr[0]); // a
+console.log(_numArr[1]); // b
+console.log(_numArr[2]); // c
+
+// 3，只读属性
+interface FullName1 {
+  readonly firstName: string;
+  lastName: string
+}
+
+const _name: FullName1 = {
+  firstName: 'kevin',
+  lastName: 'liao'
+};
+
+_name.lastName = 'Tooooom';
+console.log(_name.lastName); // Tooooom
+// _name.firstName = 'ian'; // Cannot assign to 'firstName' because it is a read-only property.
+// console.log(_name.firstName); // kevin
+
+// 4，只读数组
+const arr4: ReadonlyArray<number> = [1, 2, 3, 4, 5];
+// arr4[0] = 99; // 报错：Index signature in type 'readonly number[]' only permits reading.
+
+// 5，函数接口
+interface SumInterface {
+  (a: number, b?: number): number
+}
+
+function sum({ x, y }: { x: number, y: number }): number {
+  return x + y;
+}
+
+const sumFun: SumInterface = function (x: number, y: number | undefined): number {
+  return x + (y ? y : 0);
+}
+
+// 6，混合类型接口：约定的内容里既有属性又有方法
