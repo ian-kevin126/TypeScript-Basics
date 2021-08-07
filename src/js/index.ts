4f63e403ca8b8cdd96fef4ff30acf3f3012af488
ian_kevin126 <ian_kevin126@126.com>
2021-08-07 15:35:50 +0800

    feat: add index

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,13 @@
-console.log(1111)
+// 1，接口合并：定义多个同名接口，编译的时候会产生接口合并的现象
+interface TestInterface {
+  name:string
+}
+
+interface TestInterface {
+  age: number
+}
+
+// 类型“TestDemo”缺少类型“TestInterface”中的以下属性: name, age
+// class TestDemo implements TestInterface {}
+
+// 2，数字和字符串枚举
